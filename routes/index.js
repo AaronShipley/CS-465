8c6b0926abd01019e510e10cc87af5a9681c19f9
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-02 20:30:35 -0500

    deleted:    app_server/conrollers/main.js 	deleted:    app_server/conrollers/travel.js 	deleted:    app_server/routes/index.js 	deleted:    app_server/views/layouts/layout.hbs 	deleted:    app_server/views/partials/footer.hbs 	deleted:    app_server/views/partials/header.hbs 	deleted:    app_server/views/travel.hbs 	modified:   package-lock.json 	modified:   package.json 	renamed:    app_server/routes/travel.js -> routes/index.js 	renamed:    app_server/routes/users.js -> routes/users.js 	renamed:    app_server/views/error.hbs -> views/error.hbs 	renamed:    app_server/views/index.hbs -> views/index.hbs 	renamed:    app_server/views/layout.hbs -> views/layout.hbs

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 var express = require('express');
 var router = express.Router();
-const controller = require('../controllers/travel.js');
 
 /* GET home page. */
-router.get('/', controller.travel);
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
 
 module.exports = router;
