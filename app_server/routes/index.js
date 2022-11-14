1baa6de1165f603ea1c5f33fbf83f6c4cda3bc14
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-14 16:27:16 -0600

    modified:   app_server/controllers/about.js 	modified:   app_server/controllers/contact.js 	modified:   app_server/controllers/main.js 	modified:   app_server/controllers/meals.js 	modified:   app_server/controllers/news.js 	modified:   app_server/controllers/rooms.js 	modified:   app_server/routes/about.js 	modified:   app_server/routes/contact.js 	modified:   app_server/routes/index.js

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -3,6 +3,7 @@ var router = express.Router();
 const controller = require('../controllers/main');
 
 /* GET home page. */
-router.get('/', controller.index);
+router.get('/', controller.main);
 
 module.exports = router;
+
