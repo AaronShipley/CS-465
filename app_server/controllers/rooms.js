1154011b183242f55b9d12a871b4262c748bcbcf
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-16 18:38:36 -0600

    new file:   app_server/controllers/about.js 	new file:   app_server/controllers/contact.js 	new file:   app_server/controllers/meals.js 	new file:   app_server/controllers/news.js 	new file:   app_server/controllers/rooms.js 	new file:   app_server/routes/about.js 	new file:   app_server/routes/contact.js 	new file:   app_server/routes/meals.js 	new file:   app_server/routes/news.js 	new file:   app_server/routes/rooms.js 	new file:   app_server/views/about.hbs 	new file:   app_server/views/contact.hbs 	renamed:    app_server/views/layouts/layout.hbs -> app_server/views/layout.hbs 	new file:   app_server/views/meals.hbs 	new file:   app_server/views/news.hbs 	modified:   app_server/views/partials/footer.hbs 	modified:   app_server/views/partials/header.hbs 	new file:   app_server/views/rooms.hbs 	modified:   app_server/views/travel.hbs 	new file:   data/beds.json 	new file:   data/food.json

---
+++ app_server/controllers/rooms.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+const bed = JSON.parse(fs.readFileSync('./data/beds.json','utf8'));
+
+const rooms = (req, res) => {
+    res.render('rooms', {title: 'Rooms', bed});
+};
+module.exports = {
+    rooms
+};
