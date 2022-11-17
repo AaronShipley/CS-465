e86f90906181eb41c49cdb07d9fd36722b15705e
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-16 20:49:07 -0600

    modified:   app_server/database/models/travlr.js

--- app_server/database/models/travlr.js
+++ app_server/database/models/travlr.js
@@ -17,7 +17,16 @@ const mealSchema = new mongoose.Schema({
     type: { type: String, required: true, index: true },
     image: { type: String, required: true },
     description: { type: String, required: true }
-})
+});
+
+const roomsSchema = new mongoose.Schema({
+    name: { type: String, required: true, index: true },
+    image: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: String, required: true }
+    
+});
 
 module.exports = mongoose.model("trips", tripSchema);
 module.exports = mongoose.model("food", mealSchema);
+module.exports = mongoose.model("beds", roomsSchema);
