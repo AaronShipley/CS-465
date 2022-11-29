ffc5d777a782839be300e3b9b9769ab7bc5ce9b5
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-28 19:16:27 -0600

    modified:   app_admin/src/app/trip-card/trip-card.component.ts 	deleted:    app_admin/src/app/services/trip-data.service.ts

--- app_admin/src/app/trip-card/trip-card.component.ts
+++ app_admin/src/app/trip-card/trip-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-trip-card',
@@ -9,6 +9,7 @@ export class TripCardComponent implements OnInit {
 
   constructor() { }
 
+  @Input('trip') trip: any;
   ngOnInit() {
   }
 
