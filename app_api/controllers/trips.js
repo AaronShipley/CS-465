e1da499c6e4748f21db206b5372b45249d0e8b55
Aaron Shipley <aaron.shipley@snhu.edu>
2022-11-29 16:21:04 -0600

    modified:   app_api/controllers/trips.js

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -54,48 +54,45 @@ const tripsAddTrip = async (req, res) => {
 };
 
 const tripsUpdateTrip = async (req, res) => {
-    getUser(req, res,
-        (req, res) => {
-            
-            Model  
-                .findOneAndUpdate({ 'code': req.params.tripCode }, {
-                    code: req.body.code,
-                    name: req.body.name,
-                    length: req.body.length,
-                    start: req.body.start,
-                    resort: req.body.resort,
-                    perPerson: req.body.perPerson,
-                    image: req.body.perPerson,
-                    image: req.body.image,
-                    description: req.body.description
-                }, {new: true})
-                
-                .then(trip => {
-                    if (!trip) {
-                        return res
-                            .status(404)
-                            .send({
-                                message: "Trip not found with code " + req.params.tripCode
-                            });
-                    }
-                    return res
-                        
-        
-                }).catch(err => {
-                    if (err.kind === 'ObjectId') {
-                        return res
-                            .status(404)
-                            .send({
-                                message: "Trip not found with code " + req.params.tripCode
-                            });
-                    }
-                    return res
-                        .status(500) // server error
-                        .json(err);
-                })
-                console.log("completed updateTrip");
-        })
+  Model
+    .findOneAndUpdate({ 'code': req.params.tripCode }, {
+      code: req.body.code,
+      name: req.body.name,
+      length: req.body.length,
+      start: req.body.start,
+      resort: req.body.resort,
+      perPerson: req.body.perPerson,
+      image: req.body.perPerson,
+      image: req.body.image,
+      description: req.body.description
+    }, { new: true })
+
+    .then(trip => {
+      if (!trip) {
+        return res
+          .status(404)
+          .send({
+            message: "Trip not found with code " + req.params.tripCode
+          });
+      }
+      return res
+
+
+    }).catch(err => {
+      if (err.kind === 'ObjectId') {
+        return res
+          .status(404)
+          .send({
+            message: "Trip not found with code " + req.params.tripCode
+          });
+      }
+      return res
+        .status(500) // server error
+        .json(err);
+    })
+  console.log("completed updateTrip");
 }
+
 
 
 module.exports = {
