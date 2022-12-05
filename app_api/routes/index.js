6c5ff0636816db8f46523dada7e38c010e541b02
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-04 19:29:55 -0600

    modified:   app_api/routes/index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,27 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
 
+const authController = require('../controllers/authentication');
+const tripsController = require('../controllers/trips');
 
-
-const authController = require("../controllers/authentication");
-const tripsController = require("../controllers/trips");
-
-router.route("/login")
+router
+    .route('/login')
     .post(authController.login);
 
 router
-    .route("/register").post(authController.register);
+    .route('/register')
+    .post(authController.register);
 
 router
-  .route("/trips")
-  .get(tripsController.tripsList)
-  .post(tripsController.tripsAddTrip);
+    .route('/trips')
+    .get(tripsController.tripsList)
+    .post(tripsController.tripsAddTrip);
 
 router
-  .route("/trips/:tripCode")
-  .get(tripsController.tripsList)
-  .put(tripsController.tripsUpdateTrip);
-
-router.route("/trips/:tripCode").get(tripsController.tripsFindCode);
+    .route('/trips/:tripCode')
+    .get(tripsController.tripsFindCode)
+    .put(tripsController.tripsUpdateTrip);
 
 module.exports = router;
