2ed15668f4970e39a43725f4cf31c648ccdc3906
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-05 22:05:38 -0600

    modified:   app_api/config/passport.js 	modified:   app_api/controllers/authentication.js 		modified:   app_api/database/db.js 	modified:   app_api/database/models/travlr.js 	modified:   app_api/database/models/user.js 	modified:   app_api/routes/index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,32 +1,30 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const {expressjwt: jwt} = require('express-jwt');
 
-const authController = require('../controllers/authentication')
-const tripsController = require('../controllers/trips');
+const jwt = require('express-jwt');
+const auth = jwt({
+  secret: process.env.JWT_SECRET,
+  userProperty: "payload",
+  algorithms: ["HS256"],
+});
 
-router
-    .route('/login')
-    .post(authController.login);
+const authController = require("../controllers/authentication");
+const tripsController = require("../controllers/trips");
 
-router
-    .route('/register')
-    .post(authController.register);
+router.route("/login").post(authController.login);
 
-
-router
-    .route('/trips')
-    .get(tripsController.tripsList)
-    .post(tripsController.tripsAddTrip);
+router.route("/register").post(authController.register);
 
 router
-    .route('/trip/:tripCode')
-    .get(tripsController.tripsFindCode)
-    .put(tripsController.tripsUpdateTrip)
-    
+  .route("/trips")
+  .get(tripsController.tripsList)
+  .post(auth, tripsController.tripsAddTrip);
 
 router
-    .route("/trips/:tripCode")
-    .get(tripsController.tripsFindCode);
+  .route("/trips/:tripCode")
+  .get(tripsController.tripsList)
+  .put(auth, tripsController.tripsUpdateTrip);
+
+router.route("/trips/:tripCode").get(tripsController.tripsFindCode);
 
 module.exports = router;
