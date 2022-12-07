ecc24f5da682247cffb0eb89f01ab4b80986cbcc
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-06 20:54:16 -0600

    modified:   app_api/controllers/trips.js

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -1,9 +1,10 @@
 const { model } = require("mongoose");
 const mongoose = require("mongoose");
-const Model = mongoose.model("trips");
+const Trip = mongoose.model("trips");
+const User = mongoose.model("users");
 
 const tripsList = async (req, res) => {
-  Model.find({}).exec((err, trips) => {
+  Trip.find({}).exec((err, trips) => {
     if (!trips) {
       return res.status(404).json({ message: "trips not found" });
     } else if (err) {
@@ -15,7 +16,7 @@ const tripsList = async (req, res) => {
 };
 
 const tripsFindCode = async (req, res) => {
-  Model.find({ code: req.params.tripCode }).exec((err, trip) => {
+  Trip.find({ code: req.params.tripCode }).exec((err, trip) => {
     if (!trip) {
       return res.status(404).json({ message: "trip not found" });
     } else if (err) {
@@ -28,7 +29,7 @@ const tripsFindCode = async (req, res) => {
 
 const tripsUpdateTrip = async (req, res) => {
   console.log(req.body);
-  Model.findOneAndUpdate(
+  Trip.findOneAndUpdate(
     { code: req.params.tripCode },
     {
       code: req.body.code,
@@ -63,30 +64,35 @@ const tripsUpdateTrip = async (req, res) => {
 };
 
 const tripsAddTrip = async (req, res) => {
-  Model.create(
-    {
-      code: req.body.code,
-      name: req.body.name,
-      length: req.body.length,
-      start: req.body.start,
-      resort: req.body.resort,
-      perPerson: req.body.perPerson,
-      image: req.body.image,
-      description: req.body.description,
-    },
-    (err, trip) => {
-      if (err) {
-        return res
-          .status(400) //bad request
-          .json(err);
-      } else {
-        return res
-          .status(201) //creates
-          .json(trip);
-      }
+  console.log('tripsAddTrip invoked with:\n'+ req.body);
+  getUser(req, res,
+      (req, res) => {
+  Trip
+    .create(
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      },
+      (err, trip) => {
+        if (err) {
+          return res
+            .status(400) //bad request
+            .json(err);
+        } else {
+          return res
+            .status(201) //creates
+            .json(trip);
+        }
+      });
     }
-  );
-};
+  )
+}
 
 module.exports = {
   tripsList,
