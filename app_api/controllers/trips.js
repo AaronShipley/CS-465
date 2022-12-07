a4e0c8e83eba938a853157b88bfc129ad27b394b
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-06 20:57:26 -0600

    modified:   app_api/controllers/trips.js

--- app_api/controllers/trips.js
+++ app_api/controllers/trips.js
@@ -29,39 +29,44 @@ const tripsFindCode = async (req, res) => {
 
 const tripsUpdateTrip = async (req, res) => {
   console.log(req.body);
-  Trip.findOneAndUpdate(
-    { code: req.params.tripCode },
-    {
-      code: req.body.code,
-      name: req.body.name,
-      length: req.body.length,
-      start: req.body.start,
-      resort: req.body.resort,
-      perPerson: req.body.perPerson,
-      image: req.body.image,
-      description: req.body.description,
-    },
-    { new: true }
+  getUser(req, res,
+    (req, res) => {
+  Trip
+    .findOneAndUpdate(
+      { code: req.params.tripCode },
+      {
+        code: req.body.code,
+        name: req.body.name,
+        length: req.body.length,
+        start: req.body.start,
+        resort: req.body.resort,
+        perPerson: req.body.perPerson,
+        image: req.body.image,
+        description: req.body.description,
+      },
+      { new: true }
+    )
+      .then((trip) => {
+        if (!trip) {
+         return res.status(404).send({
+           message: "Trip not found with code " + req.params.tripCode,
+         });
+        }
+        res.send(trip);
+      })
+      .catch((err) => {
+        if (err.kind === "ObjectId") {
+          return res.status(404).send({
+          message: "Trip not found with code " + req.params.tripCode,
+          });
+        }
+        return res
+          .status(500) // server error
+          .json(err);
+      });
+    }
   )
-    .then((trip) => {
-      if (!trip) {
-        return res.status(404).send({
-          message: "Trip not found with code " + req.params.tripCode,
-        });
-      }
-      res.send(trip);
-    })
-    .catch((err) => {
-      if (err.kind === "ObjectId") {
-        return res.status(404).send({
-          message: "Trip not found with code " + req.params.tripCode,
-        });
-      }
-      return res
-        .status(500) // server error
-        .json(err);
-    });
-};
+}
 
 const tripsAddTrip = async (req, res) => {
   console.log('tripsAddTrip invoked with:\n'+ req.body);
