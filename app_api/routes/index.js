2fc916231527e92061b1c7ca637e00de2160eae2
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-08 17:17:43 -0600

    modified:   app_api/routes/index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -7,7 +7,7 @@ const { expressjwt: jwt } = require("express-jwt");
 
 const auth  = jwt({
     secret: process.env.JWT_SECRET,
-    userProperty: "payload",
+    userProperty: 'payload',
     algorithms: ["HS256"],
   });
 
