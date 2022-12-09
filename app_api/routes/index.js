997b85fa13023d74896e9a28b545fbdc842af71a
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-09 17:53:19 -0600

    modified:   app_api/routes/index.js

--- app_api/routes/index.js
+++ app_api/routes/index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {expressjwt: jwt} = ('express-jwt');
 
-const auth = jwt({
-    secret: process.env.JWT_SECRET,
-    userProperty: "payload",
-    algorithms: ["HS256"],
-  });
 
 const tripsController = require('../controllers/trips');
 const authController = require('../controllers/authentication');
