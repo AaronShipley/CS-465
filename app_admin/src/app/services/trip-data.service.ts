219055b05125822f815fe5e0c2aeb307dfaddcee
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-09 20:16:54 -0600

    deleted:    app_admin/src/app/models/authresponse.ts 	deleted:    app_admin/src/app/models/user.ts 	deleted:    app_admin/src/app/navbar/navbar.component.css 	deleted:    app_admin/src/app/navbar/navbar.component.html 	deleted:    app_admin/src/app/navbar/navbar.component.ts 	deleted:    app_admin/src/app/services/authentication.service.ts 	modified:   app_admin/src/app/services/trip-data.service.ts

--- app_admin/src/app/services/trip-data.service.ts
+++ app_admin/src/app/services/trip-data.service.ts
@@ -1,16 +1,13 @@
 import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
-import { User } from '../models/user';
-import { AuthResponse } from '../models/authresponse';
-import { BROWSER_STORAGE } from '../../../storage';
 import { Trip } from '../models/trip';
 import { response } from 'express';
 
 @Injectable()
 export class TripDataService {
 
-  constructor(private http: Http,
-    @Inject(BROWSER_STORAGE) private storage: Storage) { }
+  constructor(private http: Http) { }
+    
 
   private apiBaseUrl = 'http://localhost:3000/api/';
   private tripUrl = `${this.apiBaseUrl}trips/`;
@@ -57,21 +54,6 @@ export class TripDataService {
     return Promise.reject(error.message || error);
   }
 
-  public login(user: User): Promise<AuthResponse> {
-    return this.makeAuthApiCall('login', user);
-  }
-
-  public register(user: User): Promise<AuthResponse> {
-    return this.makeAuthApiCall('register', user);
-  }
-
-  private makeAuthApiCall(urlPath: string, user:User): Promise<AuthResponse> {
-    const url: string = `${this.apiBaseUrl}/${urlPath}`;
-    return this.http
-      .post(url, user)
-      .toPromise()
-      .then(response => response.json() as AuthResponse)
-      .catch(this.handleError);
-  }
+  
   
 }
