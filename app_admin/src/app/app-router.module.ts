d01c370a4802b49c478414ebe6c0e6d8ffc87950
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-09 23:30:58 -0600

    modified:   app_admin/src/app/app-router.module.ts

--- app_admin/src/app/app-router.module.ts
+++ app_admin/src/app/app-router.module.ts
@@ -10,7 +10,7 @@ const routes: Routes = [
     { path: 'add-trip', component: AddTripComponent },
     { path: 'edit-trip', component: EditTripComponent },
     { path: 'login', component: LoginComponent},
-    { path: 'list-trips', component: TripListingComponent},
+    { path: 'trip-listing', component: TripListingComponent},
     { path: '', component: HomeComponent, pathMatch: 'full' }
 ]
 
