28551d3c5c22d2af16c6e8eb1b07c831c225e505
Aaron Shipley <aaron.shipley@snhu.edu>
2022-12-10 21:45:59 -0600

    renamed:    app_admin/src/app/services/authentication.ts -> app_admin/src/app/authentication.ts 	modified:   app_admin/src/app/login/login.component.ts 	modified:   app_admin/src/app/navbar/navbar.component.ts 	new file:   app_admin/src/app/services/authentication.service.ts 	modified:   app_admin/src/app/trip-card/trip-card.component.ts 	modified:   app_admin/src/app/trip-listing/trip-listing.component.ts

--- app_admin/src/app/login/login.component.ts
+++ app_admin/src/app/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthenticationService } from
-  '../services/authentication';
+  '../services/authentication.service';
 import { User } from '../models/user';
 @Component({
   selector: 'app-login',
